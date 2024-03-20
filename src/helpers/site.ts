1854df399e83f3630585c2dabcba772801d93bf8
wan <47422904+safwanAzman@users.noreply.github.com>
2024-03-20 09:49:30 +0800

    Fix

--- src/helpers/site.ts
+++ src/helpers/site.ts
@@ -5,8 +5,7 @@ export const siteConfig: SiteConfig = {
     description:`
     Greetings from Ryuk Generate! This site was built by SafwanAzman with the goal of transcribing audio files into accurate text.
     This web application uses Hugging Face openai/whisper-large-v3 model to translate audio to text.
-    Feel free to explore Ryuk Generate and make use of its conveniences. Happy generating!
-    `,
+    Feel free to explore Ryuk Generate and make use of its conveniences. Happy generating!`,
     url: `https://ryuk-generate.vercel.app/`,
     ogImage: `https://ucarecdn.com/5e404a94-a135-4826-b56b-c0613af9a04e/og.jpeg`,
     name:'SafwanAzman',
